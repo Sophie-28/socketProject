7e623ccfcfd4aab0fa42526094598b3975c33b6f
Sophie-28 <76648391+Sophie-28@users.noreply.github.com>
2021-12-24 22:29:54 +0200

    Update sthread.c

--- sthread.c
+++ sthread.c
@@ -1,28 +1,3 @@
-/* echo_server_thread.c
- *
- * Copyright (c) 2000 Sean Walton and Macmillan Publishers.  Use may be in
- * whole or in part in accordance to the General Public License (GPL).
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
-*/
-
-/*****************************************************************************/
-/*** echo_server_thread.c                                                  ***/
-/***                                                                       ***/
-/*** An echo server using threads.                                         ***/
-/***                                                                       ***/
-/*** Compile : gcc echo_server_thread.c -o echo_server_thread -lpthread    ***/
-/*****************************************************************************/
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
@@ -33,23 +8,25 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <string>
-#include<iostream>
-#include<vector>
-#include<sstream>
-#include<fstream>
-
-/* Definations */
+#include <iostream>
+//#include <vector>
+#include <sstream>
+#include <fstream>
+#include <dirent.h> 
+#include <stdio.h> 
+#include <sys/stat.h>
 #define DEFAULT_BUFLEN 1024
-#define PORT 1089
+#define PORT 1090
+
 void PANIC(char* msg);
 #define PANIC(msg)  { perror(msg); exit(-1); }
 using namespace std;
 
-string check(string val);
+void check(string val);
+void list(void);
 void useracc(string val,string val2);
-/*--------------------------------------------------------------------*/
-/*--- Child - echo server                                         ---*/
-/*--------------------------------------------------------------------*/
+
+//void useracc(string val,string val2);
 
 void* Child(void* arg)
 {   char line[DEFAULT_BUFLEN];
@@ -63,13 +40,8 @@ void* Child(void* arg)
     	
         bytes_read = recv(client, line, sizeof(line), 0);
         if (bytes_read > 0) {
-        //	printf(line );  
-		//cout<<line;   
 		check(line);
-	//	string h=line;
-
-
-                if ( (bytes_read=send(client, line, bytes_read, 0)) < 0 ) {
+     if ( (bytes_read=send(client, line, bytes_read, 0)) < 0 ) {
                         printf("Send failed\n");
                     
                         break;
@@ -85,101 +57,6 @@ void* Child(void* arg)
     close(client);
     return arg;
 }
-string check(string val)
-{
-	//	string line;
-		string he;
-		vector<string> results;
-		
-	//	cout<<val;
-		for(int i=0;i<val.length();i++)
-		{
-			if(val[i]==' ')
-			{
-				he+=",";
-			}
-			else he+=val[i];
-		}
-		stringstream ss(he);
-			while(ss.good())
-				{
-					string b;
-					getline(ss,b,',');
-					results.push_back(b);
-		
-				}
-				//line=b;
-				string arresults[results.size()];
-			for(int i=0;i<results.size();i++)
-			{
-	//cout<<results.at(i)<<"\n";
-	arresults[i]=results[i];
-	cout<<arresults[i]<<"\n";
-	}
-			
-		if(arresults[0]=="USER")
-	{
-		cout<<"user";
-		string name=arresults[1];
-		string pass=arresults[2];		
-		useracc(name,pass);
-		//cout<<name<<pass<<"**";
-	
-	}
-	if(arresults[0]=="GET")
-	{
-		cout<<"get";
-	}
-	if(arresults[0]=="LIST")
-	{
-		cout<<"list";
-	}
-}
-
-void useracc(string val,string val2)
-{
-//	cout<<"stage0";	
-	ifstream file2("//home//student//21810009//pass.txt");
-		if(!file2.is_open())
-		{
-			cout<<"Failed to open\n";
-		}	
-	//	cout<<"stage1";	
-			string b,a;
-			string line;
-			int i=0;
-			while(file2.good())
-			{
-				cout<<"stage2";	
-				while(getline(file2, line))
-				{
-					cout<<"stage3";	
-					stringstream ss(line);
-					getline(ss,b,':');
-					getline(ss,a,':'); 
-				
-					if((b==val) && (a==val2))
-					{
-					
-						cout<<"user found";
-						i++;
-					}
-			}
-					cout<<"^&&^"<<b;
-					cout<<a;	
-		}
-		if(i==0)
-		{
-			cout<<"not ";
-		}
- file2.close();
-}
-
-
-/*--------------------------------------------------------------------*/
-/*--- main - setup server and await connections (no need to clean  ---*/
-/*--- up after terminated children.                                ---*/
-/*--------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {   int sd,opt,optval;
     struct sockaddr_in addr;
@@ -232,4 +109,121 @@ int main(int argc, char *argv[])
             pthread_detach(child);  /* disassociate from parent */
     }
     return 0;
+}
+void check(string val)
+{	
+//vector <string> input;
+string new_val;
+
+	for(int i=0;i<val.length();i++)
+	{
+		if(val[i]==' ')
+		
+			new_val+=",";
+		
+		else 
+		   new_val+=val[i];
+		   	
+	}
+	int index=0;
+	string hold[3];
+	stringstream ss(new_val);
+	while(ss.good())
+	{
+		string vec;
+		getline(ss,vec,',');
+		hold[index]=vec;
+		cout<<hold[index]<<"\t";
+		index++;
+	//	input.push_back(vec);
+	
+	}
+	
+/*	string array_input[input.size()];
+	for(int i=0;i<hold.size();i++)
+	{
+		array_input[i]=input[i];
+	}*/
+	if((hold[0]=="user")||(hold[0]=="USER"))
+	{
+		cout<<"user";
+			string name=hold[1];
+		string pass=hold[2];
+		useracc(name,pass);
+		
+	}
+	else if((hold[0]=="list")||(hold[0]=="LIST"))
+	{
+		cout<<"list";
+	
+			list();
+	}
+		else if((hold[0]=="get")||(hold[0]=="GET"))
+	{
+		cout<<"get";
+	
+	}
+	
+}
+void list(void){
+	 DIR *d;
+  struct dirent *dir;
+  struct stat st;
+  d = opendir(".");
+  if (d)
+  {
+  	string d_name;
+  	
+    while ((dir = readdir(d)) != NULL)
+    {
+    	int size;
+      stat(dir->d_name,&st);
+      size=st.st_size;
+    cout<< dir->d_name<<" "<<size<<"\n";
+    }
+
+    closedir(d);
+  }
+}
+void useracc(string val,string val2)
+{
+//	cout<<"stage0";	
+	ifstream file2("/home/student/21810009/pass.txt");
+		if(!file2.is_open())
+		{
+			cout<<"Failed to open\n";
+		}	
+	//	cout<<"stage1";	
+			string b,a;
+			string line;
+			int i=0;
+			while(file2.good())
+			{
+				//cout<<"stage2";	
+				while(getline(file2, line))
+				{
+					cout<<"stage3";	
+					stringstream ss(line);
+					getline(ss,b,':');
+					getline(ss,a,':'); 
+						cout<<"\n^&&^"<<b;
+					cout<<a;
+				
+					if((b==val) && (a==val2))
+					{
+					
+						cout<<"userfound\n";
+						i++;
+							cout<<line<<"\n";
+					}
+				
+			}
+				
+						
+		}
+		if(i==0)
+		{
+			cout<<"not ";
+		}
+ file2.close();
 }
